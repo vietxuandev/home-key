7a1ac7744ba8b05727896e1675188c5eb3dd4551
Long Nguyễn Thành <vietxuandev@gmail.com>
2020-05-30 02:16:07 +0700

    update

--- app/containers/RechargePage/index.js
+++ app/containers/RechargePage/index.js
@@ -4,8 +4,9 @@
  *
  */
 
-import React, { memo, useEffect } from 'react';
+import React, { memo, useEffect, useLayoutEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
 import { connect } from 'react-redux';
 import { Helmet } from 'react-helmet';
 import { Formik, Form, Field } from 'formik';
@@ -19,10 +20,19 @@ import { useInjectReducer } from 'utils/injectReducer';
 import makeSelectRechargePage from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import { TextField, InputAdornment, Button, Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import {
+  TextField,
+  InputAdornment,
+  Button,
+  Grid,
+  Container,
+  Paper,
+  Typography,
+} from '@material-ui/core';
 import NumberFormat from 'react-number-format';
 import { postRecharge } from './actions';
+import Bacground from '../../images/background.jpg';
+
 function NumberFormatCustom(props) {
   const { inputRef, onChange, ...other } = props;
 
@@ -44,82 +54,110 @@ function NumberFormatCustom(props) {
   );
 }
 
-const useStyles = makeStyles(theme => ({
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
-
 const validateForm = Yup.object().shape({
   amount: Yup.string().required('Vui lòng nhập số tiền cần nạp'),
 });
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    background: `url(${Bacground}) no-repeat center center fixed`,
+    backgroundSize: 'cover',
+  },
+  title: {
+    marginBottom: 10,
+  },
+  profile: {
+    padding: theme.spacing(2),
+  },
+  submit: {
+    marginTop: 15,
+  },
+}));
 
 export function RechargePage(props) {
   useInjectReducer({ key: 'rechargePage', reducer });
   useInjectSaga({ key: 'rechargePage', saga });
   const classes = useStyles();
-  // useEffect(() => {
-  //   props.postRecharge(1000000);
-  // }, []);
+  const useWindowSize = () => {
+    const [size, setSize] = useState([0, 0]);
+    useLayoutEffect(() => {
+      const updateSize = () => {
+        setSize([window.innerWidth, window.innerHeight]);
+      };
+      window.addEventListener('resize', updateSize);
+      updateSize();
+      return () => window.removeEventListener('resize', updateSize);
+    }, []);
+    return size;
+  };
+  const [width, height] = useWindowSize();
   return (
-    <div>
+    <div
+      className={classes.root}
+      style={{ height: width < 600 ? height - 56 : height - 64 }}
+    >
       <Helmet>
         <title>RechargePage</title>
         <meta name="description" content="Description of RechargePage" />
       </Helmet>
-      <Formik
-        initialValues={{
-          amount: '',
-        }}
-        enableReinitialize
-        onSubmit={env => {
-          props.postRecharge(env.amount);
-        }}
-        validationSchema={validateForm}
-      >
-        {({ values, errors, touched, handleSubmit, setFieldValue }) => (
-          <Form onSubmit={handleSubmit} className={classes.form}>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Field name="amount">
-                  {({ field }) => (
-                    <TextField
-                      label="Số tiền"
-                      variant="outlined"
-                      required
-                      helperText={touched.amount && errors.amount}
-                      fullWidth
-                      size="small"
-                      error={!!(touched.amount && errors.amount)}
-                      {...field}
-                      InputProps={{
-                        inputComponent: NumberFormatCustom,
-                        endAdornment: (
-                          <InputAdornment position="end">NVĐ</InputAdornment>
-                        ),
-                      }}
-                    />
-                  )}
-                </Field>
-              </Grid>
-            </Grid>
-            <Button
-              className={classes.submit}
-              fullWidth
-              variant="contained"
-              color="primary"
-              type="submit"
-              disabled={!values.amount || !_.isEmpty(errors)}
-            >
-              Nạp tiền
-            </Button>
-          </Form>
-        )}
-      </Formik>
+      <Container style={{ paddingTop: '20px' }}>
+        <Paper elevation={3} className={classes.profile}>
+          <Typography component="h1" variant="h5" className={classes.title}>
+            Nạp tiền
+          </Typography>
+          <Formik
+            initialValues={{
+              amount: '',
+            }}
+            enableReinitialize
+            onSubmit={env => {
+              props.postRecharge(env.amount);
+            }}
+            validationSchema={validateForm}
+          >
+            {({ values, errors, touched, handleSubmit, setFieldValue }) => (
+              <Form onSubmit={handleSubmit} className={classes.form}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Field name="amount">
+                      {({ field }) => (
+                        <TextField
+                          label="Số tiền"
+                          variant="outlined"
+                          required
+                          helperText={touched.amount && errors.amount}
+                          fullWidth
+                          size="small"
+                          error={!!(touched.amount && errors.amount)}
+                          {...field}
+                          InputProps={{
+                            inputComponent: NumberFormatCustom,
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                NVĐ
+                              </InputAdornment>
+                            ),
+                          }}
+                        />
+                      )}
+                    </Field>
+                  </Grid>
+                </Grid>
+                <Button
+                  className={classes.submit}
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={!values.amount || !_.isEmpty(errors)}
+                >
+                  Nạp tiền
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Paper>
+      </Container>
     </div>
   );
 }
